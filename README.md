e61a1e2b374739318010055bc2be62e96c8baf3a
osama78assi <osama78assi@gmail.com>
2024-04-30 00:19:05 +0300

    initial

--- README.md
+++
@@ -1,4 +0,0 @@
-# My Portfolio
-here is a static website, you can see all my project from here
-feel free to take the design it's not mine in the end
-also you can take the code as it's mine :)
--- src/helper/getRandSmallChar.js
+++
@@ -1,8 +0,0 @@
-// Function To Get Random Capital Case Letter
-function getRandSmallChar() {
-  const min = 65;
-  const max = 90;
-  return String.fromCharCode(Math.floor(Math.random() * (max - min + 1) + min));
-}
-
-export default getRandSmallChar;
